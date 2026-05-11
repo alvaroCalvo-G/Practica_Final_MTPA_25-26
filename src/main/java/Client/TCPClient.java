a1fcdb1103810089b57e10afa7799c12981ca95b
Álvaro Cavlo <acalvo32536@alumnos.uemc.es>
2026-05-11 14:31:57 +0200

    Version estable

--- src/main/java/Client/TCPClient.java
+++ src/main/java/Client/TCPClient.java
@@ -8,9 +8,11 @@ public class TCPClient {
     private Socket s = null;
     private DataInputStream in = null;
     private DataOutputStream out = null;
-    private InterfazGraficaCliente vista;
-    
-    public void setVista(InterfazGraficaCliente vista) {
+    private InterfazGraficaInicio vista;
+
+    private String estado = null;
+
+    public void setVista(InterfazGraficaInicio vista) {
         this.vista = vista;
     }
 
@@ -23,12 +25,22 @@ public class TCPClient {
             s = new Socket("localhost", serverPort);
             in = new DataInputStream(s.getInputStream());
             out = new DataOutputStream(s.getOutputStream());
-            if (vista != null) vista.infoConnect("Conectado");
+            if (vista != null) {
+                vista.infoConnect("Conectado");
+            }
         } catch (UnknownHostException e) {
-            if (vista != null) vista.info("Socket:" + e.getMessage());
+            if (vista != null) {
+                vista.info("Socket:" + e.getMessage());
+            }
         } catch (IOException e) {
-            if (vista != null) vista.info("IO:" + e.getMessage());
+            if (vista != null) {
+                vista.info("IO:" + e.getMessage());
+            }
         }
+    }
+
+    public void estadoCliente() throws IOException {
+        out.writeUTF(estado);
     }
 
     public void mandarComando(Comando cadena) {
@@ -36,34 +48,44 @@ public class TCPClient {
             try {
                 out.writeUTF(cadena.toString());
                 String data = in.readUTF();
-                if (vista != null) vista.info("Servidor: " + data);
+                if (vista != null) {
+                    vista.info("Servidor: " + data);
+                }
             } catch (EOFException e) {
-                if (vista != null) vista.info("EOF:" + e.getMessage());
+                if (vista != null) {
+                    vista.info("EOF:" + e.getMessage());
+                }
             } catch (IOException e) {
-                if (vista != null) vista.info("readline:" + e.getMessage());
+                if (vista != null) {
+                    vista.info("readline:" + e.getMessage());
+                }
             }
         } else {
-            if (vista != null) vista.info("Error: No hay conexión con el servidor.");
+            if (vista != null) {
+                vista.info("Error: No hay conexión con el servidor.");
+            }
         }
     }
-    
+
     public void cerrarConexion() {
         if (s != null) {
             try {
                 s.close();
             } catch (IOException e) {
-                if (vista != null) vista.info("close:" + e.getMessage());
+                if (vista != null) {
+                    vista.info("close:" + e.getMessage());
+                }
             }
         }
     }
-    
-    public static void main(String args[]) {
+
+    public static void main(String args[]) throws IOException {
         TCPClient cliente = new TCPClient();
-        InterfazGraficaCliente iu = new InterfazGraficaCliente(cliente);
-    
+        InterfazGraficaInicio iu = new InterfazGraficaInicio(cliente);
+
         cliente.setVista(iu);
-        cliente.conectar(); 
-        
         iu.setVisible(true);
+        
+        cliente.conectar();
     }
 }
