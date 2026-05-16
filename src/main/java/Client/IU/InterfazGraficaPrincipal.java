cfd36dd168f64c3e51dc71acdcc8d30c65d9ee64
alvaro <alvarocalvogomez06@gmail.com>
2026-05-16 21:21:04 +0200

    InterfazGrafica: Mostrar y ocultar lista

--- src/main/java/Client/IU/InterfazGraficaPrincipal.java
+++ src/main/java/Client/IU/InterfazGraficaPrincipal.java
@@ -5,7 +5,7 @@ import Client.Intefaces.Informacion;
 import Client.TCPClient;
 import javax.swing.JOptionPane;
 
-public class InterfazGraficaPrincipal extends javax.swing.JFrame implements  Informacion{
+public class InterfazGraficaPrincipal extends javax.swing.JFrame implements Informacion {
 
     private TCPClient clienteLogica;
 
@@ -21,27 +21,63 @@ public class InterfazGraficaPrincipal extends javax.swing.JFrame implements  Inf
                 clienteLogica.removeListener(InterfazGraficaPrincipal.this);
             }
         });
+
+        jScrollPane1.setVisible(false);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        Lista = new javax.swing.JList<>();
+        botonLista = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        Lista.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(Lista);
+
+        botonLista.setText("Mostrar lista");
+        botonLista.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonListaActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(botonLista, javax.swing.GroupLayout.DEFAULT_SIZE, 201, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1))
+                .addContainerGap(725, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addComponent(botonLista, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 486, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(63, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void botonListaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonListaActionPerformed
+        boolean visible = jScrollPane1.isVisible();
+        jScrollPane1.setVisible(!visible);
+        botonLista.setText(visible ? "Mostrar lista" : "Ocultar lista");
+    }//GEN-LAST:event_botonListaActionPerformed
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -58,5 +94,8 @@ public class InterfazGraficaPrincipal extends javax.swing.JFrame implements  Inf
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JList<String> Lista;
+    private javax.swing.JButton botonLista;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
